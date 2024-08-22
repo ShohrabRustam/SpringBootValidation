1942b2880f5ebecf7f98823c76db8f052178fa1a
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-22 14:19:04 +0530

    Curl Controller

--- src/main/java/com/example/Curl/controller/CulrController.java
+++ src/main/java/com/example/Curl/controller/CulrController.java
@@ -3,10 +3,7 @@ package com.example.Curl.controller;
 import com.example.Curl.service.CurlService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CulrController {
@@ -16,6 +13,7 @@ public class CulrController {
     public CulrController(CurlService curlService) {
         this.curlService = curlService;
     }
+
     @GetMapping("payee-validation")
     public ResponseEntity<?> payeeValidation(){
         return ResponseEntity.ok(curlService.payeeValidation());
