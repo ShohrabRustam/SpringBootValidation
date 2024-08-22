14ac90000f4e0b92aa9bb8f31fd0a12b004442a1
shorab.rustam <shorab.rustam@mpokket.com>
2024-08-22 14:18:00 +0530

    User Repo

--- src/main/java/com/example/Curl/reposatory/UserRepository.java
+++ src/main/java/com/example/Curl/reposatory/UserRepository.java
@@ -2,7 +2,11 @@ package com.example.Curl.reposatory;
 
 import com.example.Curl.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-
+    User findByUserId(int id);
 }
